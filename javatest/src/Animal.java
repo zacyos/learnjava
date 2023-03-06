fbc22351de1ee0eaaee36627380e2d73ae7d0628
zacyos <1902740302qq.com>
2023-03-06 22:21:57 +0800

    Java接口

---
+++ javatest/src/Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+    private String name;
+    private int id;
+
+    public Animal(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void whoami() {
+        System.out.println("我是" + name + ",我的id是" + id);
+    }
+    public void asd() {
+        System.out.println("我是Animal");
+    }
+
+    public static void main(String[] args) {
+        Animal al = new Animal("ads",13);
+        al.setName("shou");
+        al.whoami();
+
+    }
+}
---
+++ javatest/src/Mouse.java
@@ -0,0 +1,11 @@
+public class Mouse extends Animal {
+    public Mouse (String name,int id){
+        super(name,id);
+    }
+
+    @Override
+    public void asd() {
+        System.out.println("woshimouse");
+        super.asd();
+    }
+}
