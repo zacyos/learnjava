9fbbf5ceef9897b17ea0e6525c014d6e3170f5b1
俞澄 <1099312164@qq.com>
2023-03-13 22:17:05 +0800

    bubble sort

---
+++ javatest/src/Application.java
@@ -0,0 +1,9 @@
+import util.sort.Sort;
+
+class Application {
+
+    public static void main(String[] args) {
+        int[] array = {3, 2, 1, 5, 4, 3};
+        Sort.bubbleSort(array);
+    }
+}
--- javatest/src/util/sort/Sort.java
+++ javatest/src/util/sort/Sort.java
@@ -1,13 +1,19 @@
 package util.sort;
 
+import java.util.Arrays;
+
 public class Sort {
-    public void bubbleSort(int[] array){
-        for(int i = 0;)
+    public static void bubbleSort(int[] array) {
+        for (int j=0;j<array.length-1;j++){
+            for (int i = 0; i < array.length - 1-j; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+        }
 
-
-
+        System.out.println(Arrays.toString(array));
     }
-
-
-
 }
