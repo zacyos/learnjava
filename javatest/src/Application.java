7f3fcb2a1b9086dfb849d7c6f0df845d50214894
zacyos <1902740302qq.com>
2023-03-19 21:01:57 +0800

    message change

--- javatest/src/Application.java
+++ javatest/src/Application.java
@@ -1,9 +1,15 @@
 import util.sort.Sort;
 
+import java.util.Arrays;
+
 class Application {
 
     public static void main(String[] args) {
         int[] array = {3, 2, 1, 5, 4, 3};
-        Sort.bubbleSort(array);
+        array = Sort.insertSort(array);
+        System.out.println(Arrays.toString(array));
     }
+
+
+
 }
--- javatest/src/util/sort/Sort.java
+++ javatest/src/util/sort/Sort.java
@@ -3,9 +3,9 @@ package util.sort;
 import java.util.Arrays;
 
 public class Sort {
-    public static void bubbleSort(int[] array) {
-        for (int j=0;j<array.length-1;j++){
-            for (int i = 0; i < array.length - 1-j; i++) {
+    public static int[] bubbleSort(int[] array) {
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1 - j; i++) {
                 if (array[i] > array[i + 1]) {
                     int temp = array[i];
                     array[i] = array[i + 1];
@@ -13,7 +13,44 @@ public class Sort {
                 }
             }
         }
-
-        System.out.println(Arrays.toString(array));
+        return array;
     }
+
+    public static int[] chooseSort(int[] array) {
+//        for (int j=0;j<array.length-1;j++){
+//            for(int i = 0;i<array.length - 1 -j;i++) {
+//                if(array[i]>array[array.length-1-j]){
+//                    int temp = array[i];
+//                    array[i] = array[array.length-1-j];
+//                    array[array.length-1-j] = temp;
+//                }
+//
+//
+//            }
+//        }
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 1 + j; i < array.length; i++) {
+                if (array[j] > array[i]) {
+                    int temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] insertSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int j = i - 1;
+            int temp = array[i];
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = temp;
+        }
+        return array;
+    }
+
 }
