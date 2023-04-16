9400eca3d681559e46bc65a218a69586abc61cd1
zacyos <1902740302qq.com>
2023-04-16 21:14:08 +0800

    change

--- javatest/src/Application.java
+++ javatest/src/Application.java
@@ -1,15 +1,39 @@
 import util.sort.Sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Objects;
 
 class Application {
 
+    //    public static void main(String[] args) {
+//        int[] array = {6, 2, 1, 5, 6, 6};
+//        array = Sort.quickSort(array,0, array.length-1);
+//        System.out.println(Arrays.toString(array));
+//    }
     public static void main(String[] args) {
-        int[] array = {6, 2, 1, 5, 6, 6};
-        array = Sort.quickSort(array,0, array.length-1);
-        System.out.println(Arrays.toString(array));
+//        ArrayList<Object> sites = new ArrayList<Object>();
+//        sites.add(123);
+//        sites.add("Runoob");
+//        sites.add("Taobao");
+//        sites.add("Weibo");
+//        System.out.println(sites);
+
+        ArrayList<String> sites = new ArrayList<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+
+        // 获取迭代器
+        Iterator<String> it = sites.iterator();
+        it.next();
+        // 输出集合中的第一个元素
+        System.out.println(it.next());
     }
 
 
-
 }
+
+
--- javatest/src/yc/runoob/SendMail.java
+++ javatest/src/yc/runoob/SendMail.java
@@ -1,21 +1,29 @@
 package yc.runoob;
 
-import javax.activation.*;
-import javax.mail.*;
-import javax.mail.internet.*;
+import java.security.GeneralSecurityException;
 import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import com.sun.mail.util.MailSSLSocketFactory;
 
-public class SendMail {
-
-    public static void main(String[] args) {
+public class SendMail
+{
+    public static void main(String [] args) throws GeneralSecurityException
+    {
         // 收件人电子邮箱
-        String to = "abcd@gmail.com";
+        String to = "1902740302@qq.com";
 
         // 发件人电子邮箱
-        String from = "web@gmail.com";
+        String from = "1099312164@qq.com";
 
-        // 指定发送邮件的主机为 localhost
-        String host = "localhost";
+        // 指定发送邮件的主机为 smtp.qq.com
+        String host = "smtp.qq.com";  //QQ 邮件服务器
 
         // 获取系统属性
         Properties properties = System.getProperties();
@@ -23,8 +31,18 @@ public class SendMail {
         // 设置邮件服务器
         properties.setProperty("mail.smtp.host", host);
 
+        properties.put("mail.smtp.auth", "true");
+        MailSSLSocketFactory sf = new MailSSLSocketFactory();
+        sf.setTrustAllHosts(true);
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.ssl.socketFactory", sf);
         // 获取默认session对象
-        Session session = Session.getDefaultInstance(properties);
+        Session session = Session.getDefaultInstance(properties,new Authenticator(){
+            public PasswordAuthentication getPasswordAuthentication()
+            {
+                return new PasswordAuthentication("1099312164@qq.com", "ikorpgjwqfhsjdib"); //发件人邮件用户名、密码
+            }
+        });
 
         try{
             // 创建默认的 MimeMessage 对象
@@ -34,8 +52,7 @@ public class SendMail {
             message.setFrom(new InternetAddress(from));
 
             // Set To: 头部头字段
-            message.addRecipient(Message.RecipientType.TO,
-                    new InternetAddress(to));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
             // Set Subject: 头部头字段
             message.setSubject("This is the Subject Line!");
@@ -45,7 +62,7 @@ public class SendMail {
 
             // 发送消息
             Transport.send(message);
-            System.out.println("Sent message successfully....");
+            System.out.println("Sent message successfully....from runoob.com");
         }catch (MessagingException mex) {
             mex.printStackTrace();
         }
