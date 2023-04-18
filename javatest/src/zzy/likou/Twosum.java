a67f38d60788a524b801a797b4874cc0943ff7fc
zacyos <1902740302qq.com>
2023-04-18 22:39:45 +0800

    twosum

---
+++ javatest/src/zzy/likou/Twosum.java
@@ -0,0 +1,29 @@
+package zzy.likou;
+
+import java.util.Arrays;
+
+public class Twosum {
+    public static int[] twoSum(int[] nums, int target) {
+
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                int targetIndex = nums[i] + nums[j];
+                if (targetIndex == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] index = new int[]{2, 5, 6, 9, 3};
+        System.out.println(Arrays.toString(Twosum.twoSum(index, 9)));
+    }
+
+
+}
+
